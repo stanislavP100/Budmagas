08bf2bcfed0d190518c348a753e606a2e09a287d
stanislavP100 <stanislav_p100@ukr.net>
2020-11-28 21:20:09 +0200

    ii

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -16,14 +16,18 @@
 package com.example.android.sunshine;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.android.sunshine.utilities.Adapter;
 import com.example.android.sunshine.utilities.NetworkUtils;
@@ -35,29 +39,33 @@ public class MainActivity extends AppCompatActivity {
 
     private Adapter mAdapter;
     private RecyclerView mNumbersList;
-
-
-
-
-
+    private CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_page); //activity_main);
 
+        cardView= findViewById(R.id.first_page_card_view_1);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getBaseContext(), ChildActivity.class);
+                startActivity(intent);
+            }
+        });
 
       //  loadWeatherData(); //на first page не праює, до по цепочці все далі грузить а вигружать нема куди (мабуть)
     }
 
 
 
-
-        private void loadWeatherData() {
-
-        new WeaterQueryTask().execute("https://chitadrita.herokuapp.com/");
-
-        }
+//
+//        private void loadWeatherData() {
+//
+//        new WeaterQueryTask().execute("https://chitadrita.herokuapp.com/");
+//
+//        }
 
 
     @Override
@@ -66,71 +74,85 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
+
+//    public void onClick() {
+//        Context context = this;
+//
+//        Intent intent=new Intent(context,ChildActivity.class);
+//
+//        startActivity(intent);
+//        // TODO (1) Create a new Activity called DetailActivity using Android Studio's wizard
+//        // TODO (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
+//        // TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
+//
+//    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         int idItemThetWasSelected=item.getItemId();
 
-        if(idItemThetWasSelected==R.id.refresh_button)
-        {
-        loadWeatherData();
-        }
+//        if(idItemThetWasSelected==R.id.refresh_button)
+//        {
+//        loadWeatherData();
+//        }
 
         return super.onOptionsItemSelected(item);
     }
 
 
 
-
-    public class WeaterQueryTask extends AsyncTask <String, Void, String[]> {
-
-
-        @Override
-        protected String[] doInBackground(String... params) {
-            /* If there's no zip code, there's nothing to look up. */
-            if (params.length == 0) {
-                return null;
-            }
-
-
-           URL weatherRequestUrl = NetworkUtils.buildUrl();
-
-            try {
-                String jsonWeatherResponse = NetworkUtils
-                       .getResponseFromHttpUrl(weatherRequestUrl);
-
-               String[] simpleJsonWeatherData= OpenWeatherJsonUtils
-                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
-
-
-                return simpleJsonWeatherData;
-
-
-            } catch (Exception e) {
-
-                e.printStackTrace();
-                return null;
-            }
-
-        }
-
-
-
-        @Override
-        protected void onPostExecute(String[] strings) {
-
-            mNumbersList = (RecyclerView) findViewById(R.id.rv_numbers);
-
-            LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
-            mNumbersList.setLayoutManager(layoutManager);
-
-            mAdapter = new Adapter(MainActivity.this,strings);
-            mNumbersList.setAdapter(mAdapter);
-
-
-
-        }
-    }
+//
+//    public class WeaterQueryTask extends AsyncTask <String, Void, String[]> {
+//
+//
+//        @Override
+//        protected String[] doInBackground(String... params) {
+//            /* If there's no zip code, there's nothing to look up. */
+//            if (params.length == 0) {
+//                return null;
+//            }
+//
+//
+//           URL weatherRequestUrl = NetworkUtils.buildUrl();
+//
+//            try {
+//                String jsonWeatherResponse = NetworkUtils
+//                       .getResponseFromHttpUrl(weatherRequestUrl);
+//
+//               String[] simpleJsonWeatherData= OpenWeatherJsonUtils
+//                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+//
+//
+//                return simpleJsonWeatherData;
+//
+//
+//            } catch (Exception e) {
+//
+//                e.printStackTrace();
+//                return null;
+//            }
+//
+//        }
+//
+//
+//
+//        @Override
+//        protected void onPostExecute(String[] strings) {
+//
+//            mNumbersList = (RecyclerView) findViewById(R.id.rv_numbers);
+//
+//            LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+//            mNumbersList.setLayoutManager(layoutManager);
+//
+//            mAdapter = new Adapter(MainActivity.this,strings);
+//            mNumbersList.setAdapter(mAdapter);
+//
+//
+//
+//        }
+//    }
 
 
 
